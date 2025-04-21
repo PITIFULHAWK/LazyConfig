ad868f9a5eec99db69cbdd325a36721cb4f9f333
PITIFULHAWK <xxxxyyyy1920@gmail.com>
2025-04-21 11:39:25 +0530

    made dotfiles visible

--- lua/plugins/snack.lua
+++ lua/plugins/snack.lua
@@ -1,20 +1,42 @@
 return {
   "folke/snacks.nvim",
+  -- make sure this loads early so the opts take effect immediately
+  priority = 1000,
+  lazy = false,
+
   opts = {
     explorer = {
       supports_live = true,
       tree = true,
       watch = true,
       diagnostics_open = false,
-      git_status_open = false,
-      follow_file = true,
+      git_status_open = true,
       auto_close = false,
       jump = { close = false },
+
+      -- <<< This makes the **explorer tree** show dotfiles by default
+      hidden = true,
+
+      -- force the scanner to include hidden files
+      command = {
+        bin = "fd",
+        args = { "-H", "-t", "f" },
+      },
+
+      matcher = {
+        sort_empty = true,
+        fuzzy = true,
+        show_hidden = true, -- also allow the matcher to pass hidden files through
+        filter = function(_)
+          return true
+        end,
+      },
+
       formatters = {
         file = { filename_only = true },
         severity = { pos = "right" },
       },
-      matcher = { sort_empty = true, fuzzy = true, show_hidden = true },
+
       win = {
         list = {
           keys = {
@@ -24,9 +46,12 @@ return {
         },
       },
     },
+
     picker = {
+      -- ensure the **explorer picker** also starts with hidden = true
       sources = {
         explorer = {
+          hidden = true, -- <<< here, so Snacks.picker.explorer honors dotfiles
           layout = {
             layout = { position = "right" },
           },
@@ -34,6 +59,7 @@ return {
       },
     },
   },
+
   keys = {
     {
       "<leader>e",
@@ -48,6 +74,24 @@ return {
         require("snacks").explorer()
       end,
       desc = "Explorer Snacks (cwd)",
+    },
+    {
+      "<leader>N",
+      desc = "Neovim News",
+      function()
+        Snacks.win({
+          file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+          width = 0.6,
+          height = 0.6,
+          wo = {
+            spell = false,
+            wrap = false,
+            signcolumn = "yes",
+            statuscolumn = " ",
+            conceallevel = 3,
+          },
+        })
+      end,
     },
   },
 }
